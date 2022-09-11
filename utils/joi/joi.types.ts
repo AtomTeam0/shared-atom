5e083a8be71eb25b8cfcf72e3652eb85f4fe231f
Edan Ofer <edan.ofer@gmail.com>
2022-09-11 12:43:32 +0300

    feat: pdf added to joi types

--- utils/joi/joi.types.ts
+++ utils/joi/joi.types.ts
@@ -7,3 +7,6 @@ export const joiEnum = (enumObj: { [k: string]: string }) =>
   Joi.string().valid(...Object.values(enumObj));
 export const joiMongoId = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
 export const joiMinioId = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
+export const joiPdfURL = Joi.string().regex(
+  /^(https?:\/\/)?www\.([\da-z.-]+)\.([a-z.]{2,6})\/[\w .-]+?\.pdf$/
+);
