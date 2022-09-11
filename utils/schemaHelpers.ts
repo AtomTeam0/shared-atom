f48e1dcddc3b802831b925d498ab38a601560fc9
Edan Ofer <edan.ofer@gmail.com>
2022-09-11 14:58:20 +0300

    feat: populatePlugin added

---
+++ utils/schemaHelpers.ts
@@ -0,0 +1,54 @@
+import * as mongoose from "mongoose";
+
+export const queryFunctionTypes = [
+  "count",
+  "find",
+  "findOne",
+  "findOneAndDelete",
+  "findOneAndRemove",
+  "findOneAndUpdate",
+  "update",
+  "updateOne",
+  "updateMany",
+];
+
+export const populatePlugin = async (
+  properties: { path: string; ref: string }[],
+  schema: mongoose.Schema,
+  doc: mongoose.Model<any>
+) => {
+  const aggregateMiddleWare = schema.pre(
+    "aggregate",
+    async function (
+      this: mongoose.Aggregate<any>,
+      next: mongoose.HookNextFunction
+    ) {
+      properties.forEach((p) => {
+        this.pipeline().unshift({
+          $lookup: {
+            from: p.ref,
+            foreignField: "_id",
+            localField: p.path,
+            as: p.path,
+          },
+        });
+      });
+      next();
+    }
+  );
+
+  const queryMiddleWares = queryFunctionTypes.map((type: string) =>
+    schema.post(
+      type,
+      async function (
+        this: mongoose.Query<typeof doc, typeof doc>,
+        next: mongoose.HookNextFunction
+      ) {
+        this.populate(properties.map((p) => p.path));
+        next();
+      }
+    )
+  );
+
+  return [aggregateMiddleWare, ...queryMiddleWares];
+};
