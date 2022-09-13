fcddb1c4ca5f0d0544b02f8e8d490989d11f66b3
Edan Ofer <edan.ofer@gmail.com>
2022-09-13 14:57:43 +0300

    fix: ports changed

--- utils/rpc/rpc.config.ts
+++ utils/rpc/rpc.config.ts
@@ -1,30 +1,18 @@
 export const RPCconfig = {
   userService: {
-    hostname: process.env.USERS_API_HOST || "http://localhost",
-    port: process.env.APPLICATION_PORT || 5006,
-    api: process.env.USERS_API || "/api/users",
-    rpcPort: +(process.env.APPLICATION_RPC_PORT || 7004),
+    rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
     rpcHostname: process.env.USERS_API_RPC_HOST || "localhost",
   },
   lessonService: {
-    hostname: process.env.USERS_API_HOST || "http://localhost",
-    port: process.env.APPLICATION_PORT || 5006,
-    api: process.env.USERS_API || "/api/users",
-    rpcPort: +(process.env.APPLICATION_RPC_PORT || 7004),
+    rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
     rpcHostname: process.env.USERS_API_RPC_HOST || "localhost",
   },
   mediaService: {
-    hostname: process.env.USERS_API_HOST || "http://localhost",
-    port: process.env.APPLICATION_PORT || 5006,
-    api: process.env.USERS_API || "/api/users",
-    rpcPort: +(process.env.APPLICATION_RPC_PORT || 7004),
+    rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
     rpcHostname: process.env.USERS_API_RPC_HOST || "localhost",
   },
   itemService: {
-    hostname: process.env.USERS_API_HOST || "http://localhost",
-    port: process.env.APPLICATION_PORT || 5006,
-    api: process.env.USERS_API || "/api/users",
-    rpcPort: +(process.env.APPLICATION_RPC_PORT || 7004),
+    rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
     rpcHostname: process.env.USERS_API_RPC_HOST || "localhost",
   },
 };
