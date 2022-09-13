f5269682f50d50efec846cf996289ceb4280ba85
Edan Ofer <edan.ofer@gmail.com>
2022-09-13 11:18:01 +0300

    fix: import fix

--- utils/schema/plugins/populatePlugin.ts
+++ utils/schema/plugins/populatePlugin.ts
@@ -1,5 +1,5 @@
 import * as mongoose from "mongoose";
-import { queryFunctionTypes } from "./schemaHelpers";
+import { queryFunctionTypes } from "../schemaHelpers";
 
 export function populatePlugin(
   schema: mongoose.Schema,
