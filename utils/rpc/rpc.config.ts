d0a3404ba7fc59b0c7692e603db8f76c4e915d98
Edan Ofer <edan.ofer@gmail.com>
2022-09-14 17:20:51 +0300

    fix: rpc envs changed

--- utils/rpc/rpc.config.ts
+++ utils/rpc/rpc.config.ts
@@ -1,18 +1,18 @@
 export const RPCconfig = {
   userService: {
     rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
-    rpcHostname: process.env.USERS_API_RPC_HOST || "localhost",
+    rpcHostname: process.env.USER_SERVICE_RPC_HOST || "localhost",
   },
   lessonService: {
     rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
-    rpcHostname: process.env.USERS_API_RPC_HOST || "localhost",
+    rpcHostname: process.env.LOSSON_SERVICE_RPC_HOST || "localhost",
   },
   mediaService: {
     rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
-    rpcHostname: process.env.USERS_API_RPC_HOST || "localhost",
+    rpcHostname: process.env.MEDIA_SERVICE_RPC_HOST || "localhost",
   },
   itemService: {
     rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
-    rpcHostname: process.env.USERS_API_RPC_HOST || "localhost",
+    rpcHostname: process.env.ITEM_COMPOSITOR_RPC_HOST || "localhost",
   },
 };
