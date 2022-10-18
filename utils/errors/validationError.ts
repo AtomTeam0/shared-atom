87bc255810293e169d41558e4eac7cf0d8e411aa
NoaGrundland <noagrun.office@gmail.com>
2022-10-18 13:19:49 +0300

    feat: new error, test answers validation

--- utils/errors/validationError.ts
+++ utils/errors/validationError.ts
@@ -28,3 +28,9 @@ export class InvalidToken extends UserError {
     super("Invalid Token", 401);
   }
 }
+
+export class InvalidAnswersTestByUser extends ServerError {
+  constructor() {
+    super("Invalid answers array length", 403);
+  }
+}
