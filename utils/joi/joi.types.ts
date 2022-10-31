e5ca5cf566936b3e17dc1e174acc9f5d0866e23f
Edan Ofer <edan.ofer@gmail.com>
2022-10-31 10:03:50 +0200

    feat: mongoIds added to joi valitation

--- utils/joi/joi.types.ts
+++ utils/joi/joi.types.ts
@@ -1,33 +1,147 @@
 import * as JoiBase from "joi";
 
-const Joi = JoiBase.extend((joi: any) => ({
+// regex
+const base64regex =
+  /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+
+const mongoIdRegex = /^[0-9a-fA-F]{24}$/;
+
+const personalIdRegex = /^[0-9]{9}$/;
+
+const pdfURLRegex = /^(http.+)(\.pdf)$/;
+
+const coordinateAxisRegex = /^-?[0-9]{1,3}(?:\.[0-9]{1,15})?$/;
+
+// extentions
+const blobExtention: JoiBase.ExtensionFactory = (joi: any) => ({
   type: "joiBlob",
   base: joi.string(),
   messages: {
     "joiBlob.invalid": "{{#label}} must be a base64 string",
   },
   validate(value, helpers) {
-    const base64regex =
-      /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
     const isValid = base64regex.test(value);
     if (!isValid) {
       return { value, errors: helpers.error("joiBlob.invalid") };
     }
     return { value };
   },
-}));
+});
 
+const mongoIdExtention: JoiBase.ExtensionFactory = (joi: any) => ({
+  type: "joiMongoId",
+  base: joi.string(),
+  messages: {
+    "joiMongoId.invalid": "{{#label}} must be a mongo id string",
+    "joiMongoId.notFound": "{{#label}} was not found in the db",
+  },
+  rules: {
+    getByIdFunc: {
+      multi: true,
+      method(func) {
+        return this.$_setFlag("func", func);
+      },
+      args: [
+        {
+          name: "func",
+          ref: true,
+          assert: (value) => typeof value === "function",
+          message: "must be a function",
+        },
+      ],
+    },
+  },
+  async validate(value, helpers) {
+    let error;
+    const isValid = mongoIdRegex.test(value);
+    if (!isValid) {
+      error = helpers.error("joiMongoId.invalid");
+    } else {
+      try {
+        const res = await helpers.schema.$_getFlag("func")(value);
+        if (!res) {
+          error = helpers.error("joiMongoId.notFound");
+        }
+      } catch (err) {
+        error = err;
+      }
+    }
+
+    if (error) {
+      return { value, errors: error };
+    }
+    return { value };
+  },
+});
+
+const mongoIdArrayExtention: JoiBase.ExtensionFactory = (joi: any) => ({
+  type: "joiMongoIdArray",
+  base: joi.array().items(joi.string()),
+  messages: {
+    "joiMongoIdArray.invalid": "{{#label}} all items must be a mongo id string",
+    "joiMongoIdArray.notFound":
+      "one or more id`s from {{#label}} was not found in the db",
+  },
+  rules: {
+    getByIdFunc: {
+      multi: true,
+      method(func) {
+        return this.$_setFlag("func", func);
+      },
+      args: [
+        {
+          name: "func",
+          ref: true,
+          assert: (value) => typeof value === "function",
+          message: "must be a function",
+        },
+      ],
+    },
+  },
+  async validate(value, helpers) {
+    let error;
+    const isValid = value.every((singleVal: string) =>
+      mongoIdRegex.test(singleVal)
+    );
+    if (!isValid) {
+      error = helpers.error("joiMongoIdArray.invalid");
+    } else {
+      try {
+        const res = await Promise.all(
+          value.map(async (id: string) => helpers.schema.$_getFlag("func")(id))
+        );
+        if (res.some((result: any) => !result)) {
+          error = helpers.error("joiMongoIdArray.notFound");
+        }
+      } catch (err) {
+        error = err;
+      }
+    }
+
+    if (error) {
+      return { value, errors: error };
+    }
+    return { value };
+  },
+});
+
+const Joi = JoiBase.extend(
+  blobExtention,
+  mongoIdExtention,
+  mongoIdArrayExtention
+);
+
+// exported types
 export const joiEnum = (enumObj: { [k: string]: string }) =>
   Joi.string().valid(...Object.values(enumObj));
 
+export const joiMongoId = (getByIdFunc: (val: string) => any) =>
+  Joi.joiMongoId().getByIdFunc(getByIdFunc);
+
 export const joiBlob = Joi.joiBlob();
 
-export const joiMongoId = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
+export const joiPersonalId = Joi.string().regex(personalIdRegex);
 
-export const joiPersonalId = Joi.string().regex(/^[0-9]{9}$/);
+export const joiPdfURL = Joi.string().regex(pdfURLRegex);
 
-export const joiPdfURL = Joi.string().regex(/^(http.+)(\.pdf)$/);
-
-export const joiCoordinateAxis = Joi.string().regex(
-  /^-?[0-9]{1,3}(?:\.[0-9]{1,15})?$/
-);
+export const joiCoordinateAxis = Joi.string().regex(coordinateAxisRegex);
