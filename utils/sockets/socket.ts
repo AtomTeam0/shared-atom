f2aa2136c93ab2315cd887a24b5bfa0c89c1bcf9
NoaGrundland <noagrun.office@gmail.com>
2022-11-01 11:14:48 +0200

    feat: add socket.io server

--- utils/sockets/socket.ts
+++ utils/sockets/socket.ts
@@ -8,11 +8,23 @@ export class Socket {
     // eslint-disable-next-line no-constructor-return
     if (Socket.socketIOServer) return Socket.socketIOServer;
     Socket.socketIOServer = new Server(server, {
-      cors: { origin: "*" },
+      cors: { origin: "*", methods: ["GET", "PUT", "POST"] },
+    });
+
+    Socket.socketIOServer.on("connection", (socket) => {
+      console.log(socket);
     });
   }
 
   public static emitEvent<T>(eventName: string, changedData?: T): void {
     Socket.socketIOServer?.emit(eventName, changedData);
+  }
+
+  public static joinRooms(socket: any, roomIds: string | string[]) {
+    socket?.join(roomIds);
+  }
+
+  public static leaveRooms(socket: any, roomsId: string | string[]) {
+    socket?.leave(roomsId);
   }
 }
