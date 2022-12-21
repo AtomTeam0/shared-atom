52cf8afa6ce6d8202ec40dd15eb65f0b5c5e64f4
Edan Ofer <edan.ofer@gmail.com>
2022-12-21 15:52:59 +0200

    fix: multer changed to get deep properties

--- utils/validators/multer.ts
+++ utils/validators/multer.ts
@@ -3,8 +3,17 @@ import * as multer from "multer";
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-export function multerMiddleware<T>(porpertyArray: Array<keyof T>) {
-  return upload.fields(
-    porpertyArray.map((property) => ({ name: property as string }))
-  );
+export function multerMiddleware<T>(
+  porpertyArray: Array<keyof T>,
+  deepPropertyArray: Array<{
+    fatherProprty: keyof T;
+    childProperty: string;
+  }> = []
+) {
+  return upload.fields([
+    ...porpertyArray.map((property) => ({ name: property as string })),
+    ...deepPropertyArray.map((property) => ({
+      name: `${property.fatherProprty as string}.${property.childProperty}`,
+    })),
+  ]);
 }
