89f35ec64d1f4a78912e5d9a9d78edc8e823cb28
Edan Ofer <edan.ofer@gmail.com>
2022-12-22 14:25:08 +0200

    fix: fixed deep property type in multer middleware

--- utils/validators/multer.ts
+++ utils/validators/multer.ts
@@ -2,8 +2,16 @@ import { config } from "../../config";
 
 const multer = require("multer");
 
+type DeepKey<T> = {
+  [K in keyof T]: T[K] extends object ? DeepKey<T[K]> : K;
+};
+
+type DeepKeyString<T> = {
+  [K in keyof DeepKey<T>]: string;
+}[keyof DeepKey<T>];
+
 export function multerMiddleware<T>(
-  porpertyArray: Array<Extract<keyof T, string>>,
+  porpertyArray: Array<Extract<DeepKeyString<T>, string>>,
   fileSize = config.multer.maxSize,
   encoding = config.multer.encoding
 ) {
