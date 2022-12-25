7585f18bcb8eae2f3aaeb37a151f28fd491e50ff
Edan Ofer <edan.ofer@gmail.com>
2022-12-25 12:43:39 +0200

    fix: multer middleware now using wrapAsyncMiddleware

--- utils/validators/multer.ts
+++ utils/validators/multer.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { config } from "../../config";
 import { UnsupportedFileType } from "../errors/validationError";
 import { PorpertyOptionalDeep } from "../helpers/types";
+import { wrapAsyncMiddleware } from "../helpers/wrapper";
 
 const multer = require("multer");
 
@@ -20,25 +21,25 @@ export function multerMiddleware<T>(
       cb: (error: Error | null, acceptFile: boolean) => void
     ) => {
       if (!fileTypes.includes(file.mimetype)) {
-        cb(new UnsupportedFileType(file.mimetype), false);
+        throw new UnsupportedFileType(file.mimetype);
       } else {
         cb(null, true);
       }
     },
   };
 
-  return async (req: Request, res: Response, next: NextFunction) => {
-    await multer(multerOption).fields(
-      porpertyArray.map((property) => ({ name: property, maxCount: 1 }))
-    );
-
-    if (req.files) {
-      Object.keys(req.files).forEach((key) => {
-        const [buffer] = (req.files as any)[key];
-        req.body[key] = buffer;
-      });
-    }
-
-    next();
-  };
+  return wrapAsyncMiddleware(
+    async (req: Request, res: Response, next: NextFunction) =>
+      multer(multerOption).fields(
+        porpertyArray.map((property) => ({ name: property, maxCount: 1 }))
+      )(req, res, () => {
+        if (req.files) {
+          Object.keys(req.files).forEach((key) => {
+            const [file] = (req.files as any)[key];
+            req.body[key] = file.buffer;
+          });
+        }
+        next();
+      })
+  );
 }
