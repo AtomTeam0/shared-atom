5b97e908e0a4bc1153de1bacffff172dda4d735a
Almog Hazan <almog6666@gmail.com>
2023-07-06 11:37:58 -0700

    fix: change auth method

--- src/utils/jwt/jwt.ts
+++ src/utils/jwt/jwt.ts
@@ -1,53 +1,56 @@
-import { Request, Response, NextFunction } from "express";
-import { AuthenticationResult, ConfidentialClientApplication } from "@azure/msal-node";
-import { Global } from "common-atom/enums/helpers/Global";
-import { config } from "../../config";
-import { InvalidToken, TokenNotProvided } from "../errors/validationError";
-import { setContext } from "../helpers/context";
-import { wrapAsyncMiddleware } from "../helpers/wrapper";
-import clickConfig from "./clickConfig";
-const options = {
-  identityMetadata: `https://${clickConfig.metadata.authority}/${clickConfig.credentials.tenantID}/${clickConfig.metadata.version}/${clickConfig.metadata.discovery}`,
-  issuer: `https://${clickConfig.metadata.authority}/${clickConfig.credentials.tenantID}/${clickConfig.metadata.version}`,
-  clientID: clickConfig.credentials.clientID,
-  audience: clickConfig.credentials.clientID,
-  validateIssuer: clickConfig.settings.validateIssuer,
-  loggingNoPII: true, // great for Debugging
-  scope: clickConfig.metadata.scope,
-  loggingLevel: clickConfig.settings.loggingLevel,
-};
-// Configure the MSAL application
-const msalConfig = {
-  auth: {
-    clientId: options.clientID,
-    authority: `https://${clickConfig.metadata.authority}/${clickConfig.credentials.tenantID}`,
-    clientSecret: config.jwt.secretKey,
-  },
-};
+import { Request, Response, NextFunction } from 'express';
+import * as passport from 'passport';
+import { BearerStrategy } from 'passport-azure-ad';
+import { config } from '../../config';
+import { Global } from 'common-atom/enums/helpers/Global';
+import { setContext } from '../helpers/context';
+import { wrapAsyncMiddleware } from '../helpers/wrapper';
+import { InvalidToken, TokenNotProvided } from '../errors/validationError';
+import clickConfig from '../jwt/clickConfig';
 
-const msalApp = new ConfidentialClientApplication(msalConfig);
+// Configure the Azure AD bearer strategy
+const azureADBearerStrategy = new BearerStrategy(
+    {
+        identityMetadata: `https://${clickConfig.metadata.authority}/${clickConfig.credentials.tenantID}/${clickConfig.metadata.version}/${clickConfig.metadata.discovery}`,
+        clientID: clickConfig.credentials.clientID,
+        validateIssuer: clickConfig.settings.validateIssuer,
+        loggingLevel: clickConfig.settings.loggingLevel,
+        passReqToCallback: false,
+    },
+    (token: any, done: any) => {
+        try {
+            if (!token) {
+                throw new TokenNotProvided();
+            }
+
+            // You can add custom validation logic here if needed
+
+            return done(null, token);
+        } catch (error) {
+            console.log('Error validating access token:', error);
+            return done(new InvalidToken('Invalid access token'));
+        }
+    }
+);
+
+// Configure Passport to use the Azure AD bearer strategy
+passport.use(azureADBearerStrategy);
 
 export const verifyToken = wrapAsyncMiddleware(
-  async (req: Request, _res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      throw new TokenNotProvided();
-    }
+    async (req: Request, res: Response, next: NextFunction) => {
+        passport.authenticate('oauth-bearer', { session: false }, (err: Error, token: Express.User | undefined) => {
+            if (err) {
+                console.log('Error validating access token:', err);
+                throw new InvalidToken('Invalid access token');
+            }
 
-    const token = authHeader.split(" ")[1];
-    const result: AuthenticationResult | null = await msalApp
-      .acquireTokenOnBehalfOf({
-        scopes: clickConfig.metadata.scope, // Replace with your API's client ID
-        oboAssertion: token,
-      })
-      .catch((error: any) => {
-        console.log('error on aquireTokenOnBehalf:', error);
-        throw new InvalidToken('Error in aquireTokenOnBehalf');
-      });
-    if (result && result.account) {
-      req.user = result.account;
-      setContext(Global.AZURE_USER, req.user);
+            if (!token) {
+                throw new TokenNotProvided();
+            }
+
+            req.user = token;
+            setContext(Global.AZURE_USER, req.user);
+            next();
+        })(req, res, next);
     }
-    next();
-  }
 );
