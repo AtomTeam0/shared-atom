fc537eea2be734912356e7a05c1ce9418ff8479d
Alon Zelinsky <alon4688@gmail.com>
2023-08-07 16:51:43 +0300

    1.1.27 Fix pagination to work with documentDB

--- src/utils/helpers/aggregation.ts
+++ src/utils/helpers/aggregation.ts
@@ -1,12 +1,38 @@
-export const paginationPipline = (skip: number, limit: number) => [
-  { $group: { _id: null, totalDocs: { $sum: 1 }, data: { $push: "$$ROOT" } } },
-  { $addFields: { page: { $sum: [{ $floor: { $divide: [skip, limit] } }, 1] } } },
-  { $unwind: "$data" },
-  { $skip: skip },
-  { $limit: limit },
-  { $group: { _id: "$_id", page: { $first: "$page" }, totalDocs: { $first: "$totalDocs" }, data: { $push: "$data" } } },
+export const paginationPipline = (
+  skip: number,
+  limit: number,
+  page: number
+) => [
+  {
+    $group: {
+      _id: null,
+      totalDocs: { $sum: 1 },
+      data: { $push: "$$ROOT" },
+    },
+  },
+  {
+    $addFields: {
+      page: { $sum: [page, 1] },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      totalDocs: 1,
+      page: 1,
+      data: { $slice: ["$data", skip, limit] },
+    },
+  },
+  {
+    $project: {
+      metadata: {
+        totalDocs: "$totalDocs",
+        page: "$page",
+      },
+      data: 1,
+    },
+  },
 ];
-
 
 export const isWithSearch = (pipeline: any) => {
   const firstPipe = pipeline[0];
