bc72d984b080eaf8e12a278cfa2f849d42b0def0
Eytan Ivan Reuven <209435924@idf.il>
2023-10-22 13:38:09 +0300

    sp-0/fix formidable bug

--- src/utils/validators/formidable.ts
+++ src/utils/validators/formidable.ts
@@ -7,21 +7,21 @@ export function formidableMiddleware() {
   // convert file into JSON object
   const mergeDataIntoJSON = (data: Record<string, any>, targetJSON: object) => {
     Object.entries(data).forEach(([key, value]) => {
-      const keys = key.split(".");
+      const keys = key.split('.');
       let current: any = targetJSON;
-      keys.forEach((k, i) => {
+      keys.map((k, i) => {
         if (i === keys.length - 1) {
           current[k] = value;
         } else {
           current[k] = current[k] || {};
           current = current[k];
         }
+        return key;
       });
     });
     return targetJSON;
   };
-
-  const modifyKey = (key: string) => key.replace("]", "").replace("[", ".");
+  const modifyKey = (key: string) => key.replace(']', '').replace('[', '.');
 
   // convert each file to minimized object
   const modifyValue = (val: Record<string, any>): string => {
@@ -33,38 +33,34 @@ export function formidableMiddleware() {
     return JSON.stringify(json);
   };
 
-  return wrapAsyncMiddleware(
-    async (req: Request, _res: Response, next: NextFunction) => {
-      const form = formidable({ multiples: true });
+  return wrapAsyncMiddleware(async (req: Request, _res: Response, next: NextFunction) => {
+    const form = formidable({ multiples: true });
 
-      // Wrap the form.parse() in a Promise
-      const parseForm = () =>
-        new Promise<void>((resolve, reject) => {
-          form.parse(req, (err: any, fields: any, files: any) => {
-            if (err) {
-              reject(err);
-            } else {
-              req.body = mergeDataIntoJSON(
-                Object.assign(
-                  {},
-                  ...Object.entries(files).map(([key, value]: any) => ({
-                    [modifyKey(key)]: modifyValue(value),
-                  }))
-                ),
-                fields
-              );
-              resolve();
-            }
-          });
+    try {
+      // Await the completion of form parsing
+      await new Promise<void>((resolve, reject) => {
+        form.parse(req, (err: any, fields: formidable.Fields, files: any) => {
+          if (err) {
+            reject(err);
+          } else {
+            req.body = {
+              ...mergeDataIntoJSON(
+                  Object.assign(
+                      {},
+                      ...Object.entries(files).map(([key, value]: any) => ({
+                        [modifyKey(key)]: modifyValue(value),
+                      }))
+                  ),
+                  fields
+              ),
+            };
+            resolve();
+          }
         });
-
-      try {
-        // Await the completion of form parsing
-        await parseForm();
-        next();
-      } catch (error) {
-        next(error);
-      }
+      });
+      next();
+    } catch (error) {
+      next(error);
     }
-  );
+  });
 }
