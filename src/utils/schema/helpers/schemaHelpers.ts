ae9cb88084f7971f9fb0de1f5f57f203cc499eae
Eytan Ivan Reuven <209435924@idf.il>
2023-11-08 13:11:45 +0200

    :sparkles: add wrapper

--- src/utils/schema/helpers/schemaHelpers.ts
+++ src/utils/schema/helpers/schemaHelpers.ts
@@ -1,4 +1,4 @@
-import {QueryWithHelpers} from "mongoose";
+import {Callback, EnforceDocument, FilterQuery, QueryOptions, QueryWithHelpers} from "mongoose";
 
 export const aggregationType = ["aggregate"];
 
@@ -26,10 +26,16 @@ type SchemaMethods<T> = {
   [methodName: string]: (options?: any) => QueryWithHelpers<Array<T>, T>;
 };
 
+type Find1 = <T,TQueryHelpers = {}, TMethods = {}>(callback?: Callback<EnforceDocument<T, TMethods>[]>) => QueryWithHelpers<Array<EnforceDocument<T, TMethods>>, EnforceDocument<T, TMethods>, TQueryHelpers, T>;
+type Find2 = <T,TQueryHelpers = {}, TMethods = {}>(filter: FilterQuery<T>, callback?: Callback<T[]>) => QueryWithHelpers<Array<EnforceDocument<T, TMethods>>, EnforceDocument<T, TMethods>, TQueryHelpers, T>;
+type Find3 = <T,TQueryHelpers = {}, TMethods = {}>(filter: FilterQuery<T>, projection?: any | null, options?: QueryOptions | null, callback?: Callback<EnforceDocument<T, TMethods>[]>) => QueryWithHelpers<Array<EnforceDocument<T, TMethods>>, EnforceDocument<T, TMethods>, TQueryHelpers, T>;
+
+type FindQuery = Find1 | Find2 | Find3;
+
 export const paginationWrapper = async <T>(
     skip: number,
     limit: number,
-    query: <TMethods>(params: {}) => SchemaMethods<T>,
+    query: FindQuery,
     params: any
 ) => {
   return query(params).skip(skip).limit(limit);
