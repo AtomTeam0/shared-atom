7e86db99d7ed3bc83e599a6495a56dfacf37b646
Eytan Ivan Reuven <209435924@idf.il>
2023-11-09 11:39:37 +0200

    :construction: add paging

--- src/utils/schema/helpers/schemaHelpers.ts
+++ src/utils/schema/helpers/schemaHelpers.ts
@@ -26,18 +26,17 @@ type SchemaMethods<T> = {
   [methodName: string]: (options?: any) => QueryWithHelpers<Array<T>, T>;
 };
 
-type Find1 = <T,TQueryHelpers = {}, TMethods = {}>(callback?: Callback<EnforceDocument<T, TMethods>[]>) => QueryWithHelpers<Array<EnforceDocument<T, TMethods>>, EnforceDocument<T, TMethods>, TQueryHelpers, T>;
-type Find2 = <T,TQueryHelpers = {}, TMethods = {}>(filter: FilterQuery<T>, callback?: Callback<T[]>) => QueryWithHelpers<Array<EnforceDocument<T, TMethods>>, EnforceDocument<T, TMethods>, TQueryHelpers, T>;
-type Find3 = <T,TQueryHelpers = {}, TMethods = {}>(filter: FilterQuery<T>, projection?: any | null, options?: QueryOptions | null, callback?: Callback<EnforceDocument<T, TMethods>[]>) => QueryWithHelpers<Array<EnforceDocument<T, TMethods>>, EnforceDocument<T, TMethods>, TQueryHelpers, T>;
+type Find1<T> = <T,TQueryHelpers = {}, TMethods = {}>(callback?: Callback<EnforceDocument<T, TMethods>[]>) => QueryWithHelpers<Array<EnforceDocument<T, TMethods>>, EnforceDocument<T, TMethods>, TQueryHelpers, T>;
+type Find2<T> = <T,TQueryHelpers = {}, TMethods = {}>(filter: FilterQuery<T>, callback?: Callback<T[]>) => QueryWithHelpers<Array<EnforceDocument<T, TMethods>>, EnforceDocument<T, TMethods>, TQueryHelpers, T>;
+type Find3<T> = <T,TQueryHelpers = {}, TMethods = {}>(filter: FilterQuery<T>, projection?: any | null, options?: QueryOptions | null, callback?: Callback<EnforceDocument<T, TMethods>[]>) => QueryWithHelpers<Array<EnforceDocument<T, TMethods>>, EnforceDocument<T, TMethods>, TQueryHelpers, T>;
 
-type FindQuery = Find1 | Find2 | Find3;
+type FindQuery<T> = Find1<T> | Find2<T> | Find3<T>;
 
 export const paginationWrapper = async <T>(
     skip: number,
     limit: number,
-    query: FindQuery,
+    query: FindQuery<T>,
     params: any
 ) => {
-  console.log(query, params, skip, limit)
   return query(params).skip(skip).limit(limit);
 };
