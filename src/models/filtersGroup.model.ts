dbdfec09e68b58b5178b036d3f2101727901d29c
Eytan Ivan Reuven <209435924@idf.il>
2023-12-19 11:33:06 +0200

    :sparkles: Add FiltersGroup

---
+++ src/models/filtersGroup.model.ts
@@ -0,0 +1,42 @@
+import * as mongoose from "mongoose";
+import {FiltersGroup} from "common-atom/interfaces/filter.interface";
+import {aggregatePlugin} from "../utils/schema/plugins/aggregatePlugin";
+import {atLeastOneInArray} from "./validators/general";
+
+const FiltersGroupSchema: mongoose.Schema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        world: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+        },
+        level: {
+            type: Number,
+            required: true,
+        },
+        filters: {
+            type: [mongoose.Types.ObjectId],
+            required: true,
+            ref: "filters",
+            validate: atLeastOneInArray<mongoose.Types.ObjectId>
+        },
+        conditionFilters: {
+            type: [mongoose.Types.ObjectId],
+            ref: "filters",
+        },
+    },
+    {
+        versionKey: false,
+        timestamps: { createdAt: false, updatedAt: false },
+    }
+);
+
+// plugins
+FiltersGroupSchema.plugin(aggregatePlugin);
+export const FiltersGroupModel = mongoose.model<FiltersGroup & mongoose.Document>(
+    "filtersGroups",
+    FiltersGroupSchema
+);
