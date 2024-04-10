0017ce49b433536b9cd7227cced621b08648c490
eytan-ivan-reuven <209435924@idf.il>
2024-04-10 17:22:21 +0300

    :poop: debug swagger

--- src/server.ts
+++ src/server.ts
@@ -72,9 +72,9 @@ export class Server {
       },
       apis: ['*/Backend/**/router.ts'],
     }
-    this.app.use('/docs', serve, setup(jsdoc(swaggerSettings)));
     console.log("I'm JSDoc result", jsdoc(swaggerSettings), "--------------------------------------------------------------------------------")
     console.log("I'm Setup result", setup(jsdoc(swaggerSettings)), "--------------------------------------------------------------------------------")
+    this.app.use('/docs', serve, setup(jsdoc(swaggerSettings)));
 
     this.initializeErrorHandler();
     this.server = http.createServer(this.app);
