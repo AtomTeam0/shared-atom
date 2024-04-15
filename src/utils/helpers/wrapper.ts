d61428cfbf2bbca0102d52ed329eb2c7d41692fb
Eilam Hasson <eilamhasson1@gmail.com>
2024-04-15 15:14:18 +0300

    🏷️ switch wrappers requests to myRequest

--- src/utils/helpers/wrapper.ts
+++ src/utils/helpers/wrapper.ts
@@ -1,4 +1,5 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Response } from "express";
+import { Request } from "common-atom/interfaces/helpers/request.type";
 
 // standard wrappers (taken from the internet) that help with error handling & more
 export const wrapValidator =
