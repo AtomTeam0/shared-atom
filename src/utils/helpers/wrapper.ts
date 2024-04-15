aaf78a621aa586c242227b1b54e603253770100c
Eilam Hasson <eilamhasson1@gmail.com>
2024-04-15 17:41:29 +0300

    🐛 use express request for other wrappers

--- src/utils/helpers/wrapper.ts
+++ src/utils/helpers/wrapper.ts
@@ -1,10 +1,10 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Response, Request as expressRequest } from "express";
 import { Request } from "common-atom/interfaces/helpers/request.type";
 
 // standard wrappers (taken from the internet) that help with error handling & more
 export const wrapValidator =
-  (func: (req: Request) => Promise<void>) =>
-  (req: Request, _res: Response, next: NextFunction): void => {
+  (func: (req: expressRequest) => Promise<void>) =>
+  (req: expressRequest, _res: Response, next: NextFunction): void => {
     func(req)
       .then(() => next())
       .catch(next);
@@ -17,7 +17,7 @@ export const wrapController =
   };
 
 export const wrapAsyncMiddleware =
-  (func: (req: Request, res: Response, next: NextFunction) => Promise<void>) =>
-  (req: Request, res: Response, next: NextFunction): void => {
+  (func: (req: expressRequest, res: Response, next: NextFunction) => Promise<void>) =>
+  (req: expressRequest, res: Response, next: NextFunction): void => {
     func(req, res, next).catch(next);
   };
