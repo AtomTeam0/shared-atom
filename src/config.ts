fc436f930b3e277c9d196136a91f523cf2d0fb33
eytan-ivan-reuven <209435924@idf.il>
2024-05-01 15:23:23 +0300

    Remove unused configs

--- src/config.ts
+++ src/config.ts
@@ -6,47 +6,4 @@ export const config = {
   jwt: {
     secretKey: process.env.SECRET_KEY || "atomTeam",
   },
-  rpc: {
-    userService: {
-      rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
-      rpcHostname: process.env.USER_SERVICE_RPC_HOST || "localhost",
-    },
-    lessonService: {
-      rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
-      rpcHostname: process.env.LESSON_SERVICE_RPC_HOST || "localhost",
-    },
-    mediaService: {
-      rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
-      rpcHostname: process.env.MEDIA_SERVICE_RPC_HOST || "localhost",
-    },
-    itemService: {
-      rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
-      rpcHostname: process.env.ITEM_COMPOSITOR_RPC_HOST || "localhost",
-    },
-    newsService: {
-      rpcPort: +(process.env.APPLICATION_RPC_PORT || 5000),
-      rpcHostname: process.env.NEWS_SERVICE_RPC_HOST || "localhost",
-    },
-  },
-  fileService: {
-    projectId: +(process.env.PROJECT_ID || 1),
-    fileNameStarter: process.env.FILE_NAME_STARTER || "mynet",
-    archiveApiKey: process.env.ARCHIVE_API_KEY || "",
-    hostname:
-        process.env.FILE_SERVICE_HOSTNAME ||
-        "https://api.digital.idf.il/external/hatch",
-    uploadRoute: process.env.UPLOAD_FILE_SERVICE_ROUTE || "func-upload",
-    downloadRoute: process.env.DOWNLOAD_FILE_SERVICE_ROUTE || "func-getFile"
-  },
-  formidable: {
-    propertyConfigs: {
-      item: ["thumbNail"],
-      unit: ["image"],
-      infographic: ["image"],
-      article: ["thumbNail", "bestSoldier.image", "pdf"],
-      lesson: ["pdf"],
-      pakal: ["pdf"],
-      media: ["video", "audio"],
-    },
-  },
 };
