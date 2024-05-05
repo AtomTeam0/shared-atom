43f5766a725373583e5fffb284b1234446c5c8d9
Eilam Hasson <eilamhasson1@gmail.com>
2024-05-05 11:28:21 +0300

    💡 comment

--- src/paths/functions.ts
+++ src/paths/functions.ts
@@ -31,6 +31,7 @@ import {
 } from "./types";
 import Paths from "./paths";
 
+//actually already exists in the nest utils, will be removed when shared is in the monorepo
 const switchValueIf =
   <Origin, Result>(
     condition: Origin | Function1<Origin, boolean>,
@@ -48,6 +49,14 @@ const switchValueIf =
     return finalResult;
   };
 
+const toURLFunction =
+  (route: string) =>
+  (...args: string[]) =>
+    flow(split("/"), map(putArgIfParam(route, args)), join("/"))(route);
+
+const isURLObject = (obj: object): obj is URLObject =>
+  "URL" in obj && isString(obj.URL) && isParamRoute(obj.URL);
+
 export const getRouters = <T extends Service>(serviceName: T) => {
   const routers = flow(
     get(serviceName),
@@ -64,36 +73,6 @@ export const getRouters = <T extends Service>(serviceName: T) => {
   };
 };
 
-const isParamRoute = (value: string): value is ParamRoute =>
-  includes("/:", value) || startsWith(":", value);
-
-const isURLObject = (obj: object): obj is URLObject =>
-  "URL" in obj && isString(obj.URL) && isParamRoute(obj.URL);
-
-const switchWithArg = (route: string, args: string[]) => (segment: string) =>
-  flow(split("/"), indexOf(segment), nth(__, args))(route)!;
-
-const putArgIfParam = (route: string, args: string[]) => (segment: string) =>
-  switchValueIf(startsWith(":"), switchWithArg(route, args))(segment);
-
-const toURLFunction =
-  (route: string) =>
-  (...args: string[]) =>
-    flow(split("/"), map(putArgIfParam(route, args)), join("/"))(route);
-
-const transformUrls = <Paths extends object>(
-  paths: Paths
-): TransformUrls<Paths> =>
-  mapValues(
-    switchValueIf(isPlainObject, updateNestedUrls),
-    paths
-  ) as TransformUrls<Paths>;
-
-const updateNestedUrls = flow(
-  switchValueIf(isURLObject, update("URL", toURLFunction)),
-  transformUrls
-);
-
 export const getPaths = <
   T extends Service,
   R extends undefined | keyof GetService<T> = undefined,
@@ -108,3 +87,32 @@ export const getPaths = <
 
   return (withParams ? transformUrls(result) : result) as FinalResult<T, R, P>;
 };
+
+//The code regarding transformUrls is a bit verbose and logic-heavy.
+//basically, the function takes a Route object ({METHOD: ... URL: ...})
+//and for parameterized routes ("/users/:id") it replaces the url with a function that,
+//given the params, returns the full url. so, for example, for the object
+//{METHOD: "get", URL: "/users/:param1/world/:param2"}, it would return something that
+//looks like: {METHOD: "get", URL: (param1, param2) => `/users/${param1}/world/${param2}`}
+//with some carefull reading, the code should be pretty understandable
+const transformUrls = <Paths extends object>(
+  paths: Paths
+): TransformUrls<Paths> =>
+  mapValues(
+    switchValueIf(isPlainObject, updateNestedUrls),
+    paths
+  ) as TransformUrls<Paths>;
+
+const updateNestedUrls = flow(
+  switchValueIf(isURLObject, update("URL", toURLFunction)),
+  transformUrls
+);
+
+const putArgIfParam = (route: string, args: string[]) => (segment: string) =>
+  switchValueIf(startsWith(":"), switchWithArg(route, args))(segment);
+
+const switchWithArg = (route: string, args: string[]) => (segment: string) =>
+  flow(split("/"), indexOf(segment), nth(__, args))(route)!;
+
+const isParamRoute = (value: string): value is ParamRoute =>
+  includes("/:", value) || startsWith(":", value);
