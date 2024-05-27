3d4dc8800edccc480382508a7e6be1a864a2bbaa
Eilam Hasson <eilamhasson1@gmail.com>
2024-05-27 14:51:38 +0300

    ♻️ fix validateRequest

--- src/utils/joi/joi.functions.ts
+++ src/utils/joi/joi.functions.ts
@@ -2,7 +2,11 @@ import { Permission } from "common-atom/enums/Permission";
 import { Global } from "common-atom/enums/helpers/Global";
 import { Request } from "express";
 import { ObjectSchema, ValidationOptions } from "joi";
-import { throwUnauthorizedError } from "../errors/ErrorGenerators";
+import { flow, get } from "lodash/fp";
+import {
+  throwBadRequestError,
+  throwUnauthorizedError,
+} from "../errors/ErrorGenerators";
 import { getContext } from "../helpers/context";
 import { wrapValidator } from "../helpers/wrapper";
 
@@ -12,40 +16,20 @@ export const defaultValidationOptions: ValidationOptions = {
   convert: true,
 };
 
-const normalizeRequest = (req: any, value: any): void => {
-  req.originalBody = req.body;
-  req.body = value.body;
-
-  req.originalQuery = req.query;
-  req.query = value.query;
-
-  req.originalParams = req.params;
-  req.params = value.params;
-};
-
 // joi validation for noraml schemas
-export const validateRequest = (
-  schema: ObjectSchema,
-  options: ValidationOptions = defaultValidationOptions,
-  doesWrap = true
-): any => {
-  const validator = async (req: Request): Promise<void> => {
-    const value = await schema.unknown().validateAsync(req, options);
-    if (options.convert) {
-      normalizeRequest(req, value);
-    }
-  };
-
-  return doesWrap ? wrapValidator(validator) : validator;
-};
+export const validateRequest =
+  (schema: ObjectSchema): any =>
+  async (req: Request) =>
+    await schema
+      .validateAsync(req)
+      .catch(flow(get("message"), throwBadRequestError));
 
 // joi validation for schemas that changes according to the permission of the user
 export const validateRequestByPermission = (
   allValidations: {
     permissions: Permission[];
     schema: ObjectSchema<any>;
-  }[],
-  options: ValidationOptions = defaultValidationOptions
+  }[]
 ) => {
   const validator = async (req: Request): Promise<void> => {
     const wantedValidation = allValidations.find((validation) =>
@@ -53,7 +37,7 @@ export const validateRequestByPermission = (
     );
 
     wantedValidation
-      ? await validateRequest(wantedValidation.schema, options, false)(req)
+      ? await validateRequest(wantedValidation.schema)(req)
       : throwUnauthorizedError("Invalid Permission");
   };
   return wrapValidator(validator);
